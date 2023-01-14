0df318cbcf6d9ec93e419627b215555535bb3415
Arjun Hadke <arjun.swcm@gmail.com>
2023-01-14 22:44:40 +0530

    initial commit plus some code additions

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.php'],
+  theme: {
+    extend: {
+      'h1': 'font-size:26px;',
+      'h1': 'font-size:24px;'
+    },
+  },
+  plugins: [],
+}
