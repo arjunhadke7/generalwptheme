fd704b4a85d89e87f0a0d2e5a6e83e9f009483aa
Arjun Hadke <arjun.swcm@gmail.com>
2023-01-14 22:58:03 +0530

    removed some files for making things easy

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
+			
 			get_template_part( 'template-parts/content', get_post_type() );
 
 			the_post_navigation(
@@ -26,9 +26,10 @@ get_header();
 			);
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
+			// comments are disabled only for testing purpose and for working on the main theme portion
 
 		endwhile; // End of the loop.
 		?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.php'],
+  content: ['*/*.php'],
   theme: {
     extend: {
       'h1': 'font-size:26px;',
